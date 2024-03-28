daf743df9b637e0047e8b745917a6324f9481600
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-03-28 16:54:09 +0100

    feat: mail exercise completed with html input and output

---
+++ dadi/README.md
@@ -0,0 +1,4 @@
+# Dadi contro il pc
+[ ] creare un bottone che una volta premuto stampa un numero casuale all'utente da 1 a 6
+[ ] stampare numero casuale creato dal pc
+[ ] Se il pc
--- mail/js/script.js
+++ mail/js/script.js
@@ -1,10 +1,27 @@
-const mailList = ["Gino", "Pino", "Tino", "Lino", "Dino"]; // creazione array
-let userInput = prompt("inserisci nome contatto"); // raccolta dati
-console.log(userInput);
+const mailList = ["gino@gmail.com", "pino@gmail.com", "tino@gmail.com", "lino@gmail.com", "dino.@gmail.com"]; // creazione array
 
-for (i = 0; i < mailList.length; i++)
-if (userInput === mailList[i]) {
-    alert("bella zio");
-} else {
-    alert("nome non presente")
-}
+const sendBtn = document.getElementById("sendbtn");
+sendBtn.addEventListener("click", function () {
+    const userInput = document.getElementById("user_email"); // raccolta dati
+    let userValue = userInput.value;
+    console.log(userValue);
+
+    let answer;
+    for (let i = 0; i < mailList.length; i++)
+        if (userValue === mailList[i]) {
+            answer = "utente registrato";
+            document.getElementById("answer_out").innerHTML = answer;      
+            console.log(answer);
+        } else {
+            answer = "utente non registrato";
+            document.getElementById("answer_out").innerHTML = answer;
+        }
+})
+
+// for (let i = 0; i < mailList.length; i++)
+// if (userInput === mailList[i]) {
+//     alert("utente registrato");
+// } else {
+//     alert("utente non registrato")
+// }
+
