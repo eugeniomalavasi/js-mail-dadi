f2dee02945a8bf41853c3f016be9adea0e49518c
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-03-28 17:41:57 +0100

    feat: ex dadi completed

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,9 +1,13 @@
+const btn = document.querySelector("body button");
+btn.addEventListener("click", function () {
+// CREAZIONE NUMERI CASUALI
 let pcNumb = Math.floor(Math.random() * 6) + 1;
 console.log(pcNumb, "pc");
-
 let userNumb = Math.floor(Math.random() * 6) + 1;
 console.log(userNumb, "user");
 let results
+
+// LOGICA
 if (pcNumb > userNumb) {
     results = "hai perso";
 } else if (pcNumb === userNumb) {
@@ -11,5 +15,9 @@ if (pcNumb > userNumb) {
 } else  {
     results = "hai vinto!"
 }
-
 console.log(results);
+    // OUTPUT
+    document.getElementById("user_number").innerHTML = `Il tuo numero è: ${userNumb}`;
+    document.getElementById("pc_number").innerHTML = `Il numero del pc è: ${pcNumb}`;
+    document.getElementById("who_won").innerHTML = results;
+})
