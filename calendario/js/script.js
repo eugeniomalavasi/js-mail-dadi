8b376cbf26699582302152289c1962237d13b413
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-03-29 14:49:41 +0100

    feat: calendar logic probably is now fine

--- calendario/js/script.js
+++ calendario/js/script.js
@@ -1,15 +1,14 @@
-const laborDays = ["lunedì", "martedì", "mercoledì", "giovedì", "venerdì"]
+const laborDays = ["lunedì", "martedì", "mercoledì", "giovedì", "venerdì", "sabato", "domenica"]
 console.log(laborDays);
-const holyDays = ["sabato", "domenica"]
-console.log(holyDays);
+// const holyDays = ["sabato", "domenica"]
+// console.log(holyDays);
 
 
-for (let i = 0; i < 6; i++) {
-    if (i < laborDays.length) {
+for (let i = 0; i < 7; i++) {
+    if (i < 5) {
         console.log(laborDays[i],"feriali");
-    } else if (i > laborDays.length) {
-        i = 0;
-        console.log(holyDays[i], "festivi");
+    } else if (i < laborDays.length && i >= 5) {
+        console.log(laborDays[i], "festivi");
     }
 }
 
