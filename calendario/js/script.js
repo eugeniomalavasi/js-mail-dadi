5eaffd12d5a3e48bee298bd6383e6d0f62632f41
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-03-29 10:58:28 +0100

    feat: calendar ex bonus started

---
+++ calendario/js/script.js
@@ -0,0 +1,27 @@
+const laborDays = ["lunedì", "martedì", "mercoledì", "giovedì", "venerdì"]
+console.log(laborDays);
+const holyDays = ["sabato", "domenica"]
+console.log(holyDays);
+
+
+for (let i = 1; i <= 31; i++) {
+    if (i < laborDays) {
+
+    }
+    console.log(i);
+
+}
+
+
+
+
+
+
+// dobbiamo stampare il calendario di Marzo
+// ci sono 31 posti da riempire
+// la settimana è divisa in giorni feriali e festivi
+// bisogna partire dal venerdì
+// mettere i giorni festivi
+// stampare i giorni feriali
+// e di nuovo i giorni festivi
+
